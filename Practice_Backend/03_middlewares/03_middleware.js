624cbbd51e2c8fcc578cee26875f950d6a11dcd8
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-05-31 23:13:00 +0530

    add a authentication middleware

--- Practice_Backend/03_middlewares/03_middleware.js
+++ Practice_Backend/03_middlewares/03_middleware.js
@@ -1,6 +1,23 @@
 //  Example 3: Route-specific Middleware
-import express from "express"
+import express from "express";
 
-const port = 3000
-const app = express()
+const port = 3000;
+const app = express();
 
+function authMiddleWare(req, res, next) {
+  const isAuthorised = false;
+  console.log("Middlware in action!");
+  if (isAuthorised) {
+    next();
+  } else {
+    res.status(404).send("Error 404 | User not authorized!");
+  }
+}
+
+app.get("/protected", authMiddleWare, (req, res) => {
+  res.send("Welcome to protected route");
+});
+
+app.listen(port, () => {
+  console.log("Server is running at port 3000...");
+});
